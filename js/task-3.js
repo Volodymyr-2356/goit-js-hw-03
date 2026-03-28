0215762e00a088edc843b230088d94c956642c89
vovangrk <vovangrk@ukr.net>
2026-03-28 15:47:38 +0200

    hw 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,17 @@
-function getElementWidth(content, padding, border) {
-    const a = parseFloat(content);
-    const b = parseFloat(padding);
-    const c = parseFloat(border);
-    return a + b * 2 + c * 2;
-   
+function filterArray(numbers, value) {
+    const a = [];
+    for (const items of numbers) {
+        if (items > value) {
+            a.push(items);
+        }
+    }
+    return a;
+
     
 }
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px"));
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
